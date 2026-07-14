c7eb5ab56170746d4f2e9a8ef78963403b765ad1
rentcas <renatas@piksel.lt>
2026-07-14 22:20:50 +0300

    Improve MVP: app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "lt.saulute.coach"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     buildFeatures { compose = true }
